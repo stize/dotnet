52e6d9ef372dfd559e52a3781f6fb59f412d6978
Sergio Moreno <sergio.m.calzada@gmail.com>
2020-11-02 20:28:37 +0100

    added IFileSystem abstraction
    added LocalFileSystemSystem implementation

---
+++ src/DotNet/FileSystem/IFileSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Stize.DotNet.FileSystem
+{
+    /// <summary>
+    /// Interface providing abstraction over the different file storage providers
+    /// </summary>
+    public interface IFileSystem
+    {
+        /// <summary>
+        /// Saves a file to the storage provider
+        /// </summary>
+        /// <param name="path">Path to save the file</param>
+        /// <param name="stream">File content</param>
+        /// <returns>Async task</returns>
+        Task SaveAsync(string path, Stream stream);
+
+        /// <summary>
+        /// Checks if a file exists in the storage provider
+        /// </summary>
+        /// <param name="path">Path to check</param>
+        /// <returns>Does the file exist in the storage provider?</returns>
+        Task<bool> ExistsAsync(string path);
+
+        /// <summary>
+        /// Deletes a file from the storage provider
+        /// </summary>
+        /// <param name="path">Path to delete</param>
+        /// <returns>Has the file been deleted from the storage provider?</returns>
+        Task<bool> DeleteAsync(string path);
+
+        /// <summary>
+        /// Opens a file
+        /// </summary>
+        /// <param name="path">Path to file in the storage provider</param>
+        /// <returns>File content</returns>
+        Task<Stream> OpenAsync(string path);
+    }
+
+}
